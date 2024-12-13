b329cfa28fd312867b435c940f8f61dc8d467bec
MeenakshiPasupuleti12 <meenakshipasupuleti628@gmail.com>
2024-12-13 11:30:26 +0530

    essageMvc files

---
+++ MVC/MVC/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult EnterNewProduct()
+        {
+            return View();
+        }
+        public ActionResult SaveProduct(Product p)
+        {
+             
+            return View("DisplayProduct", p);
+        }
+
+
+       
+    }
+}
---
+++ MVC/MVC/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Product
+    {
+        public int ProductId {  get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Quantity { get; set; }
+
+        public int Price { get; set; }
+    }
+}
